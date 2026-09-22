a46ecfdec9e01f39b1200c79243566cc79efa9a0
Roshan <jaiswalroshan920@gmail.com>
2026-09-22 16:56:59 +0530

    Add Compose theme: Color.kt

---
+++ app/src/main/java/com/example/jeecommandcenter/ui/theme/Color.kt
@@ -0,0 +1,28 @@
+package com.example.jeecommandcenter.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BgApp = Color(0xFF0B1220)
+val BgCard = Color(0xFF111C30)
+val BgCardAlt = Color(0xFF132339)
+val BgCardBorder = Color(0xFF1E3A5F)
+val BgDivider = Color(0xFF1E293B)
+val BgPriority = Color(0xFF2A1A0A)
+val BorderPriority = Color(0xFF78350F)
+val BgPriorityBadge = Color(0xFF3F1D1D)
+val TextPriorityBadge = Color(0xFFFCA5A5)
+val TextAmber = Color(0xFFFBBF24)
+val TextPrimary = Color(0xFFF8FAFC)
+val TextSecondary = Color(0xFF94A3B8)
+val TextMuted = Color(0xFF64748B)
+val TextOnCard = Color(0xFFE2E8F0)
+val AccentBlue = Color(0xFF3B82F6)
+val AccentBlueLight = Color(0xFF93C5FD)
+val AccentBlueSoft = Color(0xFF1E3A5F)
+val AccentGreen = Color(0xFF22C55E)
+val AccentGreenDark = Color(0xFF052E16)
+val AccentAmber = Color(0xFFF59E0B)
+val AccentPurple = Color(0xFFA855F7)
+val AccentPink = Color(0xFFEC4899)
+val ChipSelectedBg = AccentBlue
+val ChipUnselectedBg = Color(0xFF16233A)
