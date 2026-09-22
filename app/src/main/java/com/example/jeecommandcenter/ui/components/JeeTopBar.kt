6680fb0bf99755f535af6a24b564ac7ba1be0496
Roshan <jaiswalroshan920@gmail.com>
2026-09-22 22:53:51 +0530

    Opt in to Material 3 experimental top app bar API

--- app/src/main/java/com/example/jeecommandcenter/ui/components/JeeTopBar.kt
+++ app/src/main/java/com/example/jeecommandcenter/ui/components/JeeTopBar.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -13,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun JeeTopBar(
     title: String,
