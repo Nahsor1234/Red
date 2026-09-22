9f1194c6a438cfcfdec964d93a7d6420156f1148
Roshan <jaiswalroshan920@gmail.com>
2026-09-22 18:19:33 +0530

    Simplify haptic interaction helper

--- app/src/main/java/com/example/jeecommandcenter/ui/components/Haptics.kt
+++ app/src/main/java/com/example/jeecommandcenter/ui/components/Haptics.kt
@@ -1,14 +1,15 @@
 package com.example.jeecommandcenter.ui.components
 
 import android.view.HapticFeedbackConstants
-import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.clickable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.platform.LocalView
-import androidx.compose.foundation.ExperimentalFoundationApi
 
-@OptIn(ExperimentalFoundationApi::class)
-fun Modifier.premiumClick(onClick:()->Unit):Modifier = composed {
- val view=LocalView.current
- combinedClickable(onClick={view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP);onClick()})
+fun Modifier.premiumClick(onClick: () -> Unit): Modifier = composed {
+    val view = LocalView.current
+    clickable {
+        view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
+        onClick()
+    }
 }
